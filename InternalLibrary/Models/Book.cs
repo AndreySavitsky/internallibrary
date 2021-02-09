5c707f6ce0f09df91a2aa527c2b14f6e2818cf76
AndreySavitsky <Andrey.Savitsky@softeq.com>
2021-02-09 14:21:10 +0300

    Book model added

---
+++ InternalLibrary/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace InternalLibrary.Models
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public DateTime DateOfIssue { get; set; }
+        public string InternationalStandardBookNumber { get; set; }
+        public string Location { get; set; } = String.Empty;
+    }
+}
