e12ab3e282a2b673eb632967a8b919601c3bcd39
AndreySavitsky <Andrey.Savitsky@softeq.com>
2021-02-10 18:06:00 +0300

    Packages reimported

--- iOS/CustomIosBootstrapper.cs
+++ iOS/CustomIosBootstrapper.cs
@@ -2,12 +2,7 @@
 using System.Reflection;
 
 using Softeq.XToolkit.Common.Extensions;
-using Softeq.XToolkit.Permissions;
-using Softeq.XToolkit.Permissions.iOS;
 using Softeq.XToolkit.WhiteLabel.Bootstrapper.Abstract;
-using Softeq.XToolkit.WhiteLabel.Essentials.ImagePicker;
-using Softeq.XToolkit.WhiteLabel.Essentials.iOS.FullScreenImage;
-using Softeq.XToolkit.WhiteLabel.Essentials.iOS.ImagePicker;
 using Softeq.XToolkit.WhiteLabel.Interfaces;
 using Softeq.XToolkit.WhiteLabel.iOS;
 using Softeq.XToolkit.WhiteLabel.iOS.Services;
@@ -20,7 +15,6 @@ namespace InternalLibrary.iOS
         protected override IList<Assembly> SelectAssemblies()
         {
             return base.SelectAssemblies() // Softeq.XToolkit.WhiteLabel.iOS
-                .AddItem(typeof(FullScreenImageViewController).Assembly) // Softeq.XToolkit.WhiteLabel.Essentials.iOS
                 .AddItem(GetType().Assembly); // Playground.iOS
         }
 
@@ -30,13 +24,6 @@ namespace InternalLibrary.iOS
             builder.Singleton<IosAppInfoService, IAppInfoService>();
 
             builder.Singleton<StoryboardDialogsService, IDialogsService>();
-
-            // permissions
-            builder.Singleton<PermissionsService, IPermissionsService>();
-            builder.Singleton<PermissionsManager, IPermissionsManager>();
-
-            // image picker
-            builder.Singleton<IosImagePickerService, IImagePickerService>();
         }
     }
 }
