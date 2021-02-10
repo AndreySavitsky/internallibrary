c9bf2f798019f9fefbd266871b38a08dc9e046cc
AndreySavitsky <Andrey.Savitsky@softeq.com>
2021-02-10 14:01:12 +0300

    BookListViewController added

---
+++ iOS/ViewControllers/BookListViewController.cs
@@ -0,0 +1,14 @@
+﻿using InternalLibrary.ViewModels;
+
+using Softeq.XToolkit.WhiteLabel.iOS;
+
+namespace InternalLibrary.iOS.ViewControllers
+{
+    public class BookListViewController : ViewControllerBase<BookListViewModel>
+    {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+    }
+}
