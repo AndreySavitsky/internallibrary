2e4ea3d0fdb3af8e4e994f705f887135efae6f36
AndreySavitsky <Andrey.Savitsky@softeq.com>
2021-02-10 14:18:02 +0300

    BookListViewController recreated

--- iOS/ViewControllers/BookListViewController.cs
+++ iOS/ViewControllers/BookListViewController.cs
@@ -1,14 +1,28 @@
-﻿using InternalLibrary.ViewModels;
-
+﻿using System;
+using InternalLibrary.ViewModels;
 using Softeq.XToolkit.WhiteLabel.iOS;
+using UIKit;
 
 namespace InternalLibrary.iOS.ViewControllers
 {
-    public class BookListViewController : ViewControllerBase<BookListViewModel>
+    public partial class BookListViewController : ViewControllerBase<BookListViewModel>
     {
+        public BookListViewController(IntPtr handle) : base(handle)
+        {
+
+        }
+
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
         }
     }
 }
+
