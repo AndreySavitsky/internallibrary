b6aada978a0368bacacfffb2b0e0f5ec3f3560a7
AndreySavitsky <Andrey.Savitsky@softeq.com>
2021-02-11 12:42:50 +0300

    BookViewModel added

---
+++ InternalLibrary/ViewModels/BookViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Softeq.XToolkit.Common;
+
+namespace InternalLibrary.ViewModels
+{
+    public class BookViewModel : ObservableObject
+    {
+        private string _name = string.Empty;
+        private DateTime _dateOfIssue = default;
+        private string _internationalStandardBookNumber = string.Empty;
+        private string _location = string.Empty;
+
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+
+        public DateTime DateOfIssue
+        {
+            get => _dateOfIssue;
+            set => Set(ref _dateOfIssue, value);
+        }
+
+        public string InternationalStandardBookNumber
+        {
+            get => _internationalStandardBookNumber;
+            set => Set(ref _internationalStandardBookNumber, value);
+        }
+
+        public string Location
+        {
+            get => _location;
+            set => Set(ref _location, value);
+        }
+    }
+}
