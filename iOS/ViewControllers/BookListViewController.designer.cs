69b4ce26a5dbc2e1c1fe1a218135ecd377385710
AndreySavitsky <Andrey.Savitsky@softeq.com>
2021-02-11 12:11:03 +0300

    TableView added into BookListStoryboard

--- iOS/ViewControllers/BookListViewController.designer.cs
+++ iOS/ViewControllers/BookListViewController.designer.cs
@@ -1,6 +1,6 @@
 ﻿// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
+// This file has been generated automatically by Visual Studio from the outlets and
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
@@ -11,11 +11,19 @@ using UIKit;
 
 namespace InternalLibrary.iOS.ViewControllers
 {
-    [Register("BookListViewController")]
+    [Register ("BookListViewController")]
     partial class BookListViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITableView TableView { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
+            if (TableView != null) {
+                TableView.Dispose ();
+                TableView = null;
+            }
         }
     }
 }
