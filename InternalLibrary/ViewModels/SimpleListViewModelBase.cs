598fa4ea76e1d7584098c43c7f8acbba0331140e
AndreySavitsky <Andrey.Savitsky@softeq.com>
2021-02-11 14:29:57 +0300

    SimpleListViewModelBase added

---
+++ InternalLibrary/ViewModels/SimpleListViewModelBase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Softeq.XToolkit.Common.Collections;
+using Softeq.XToolkit.Common.Commands;
+using Softeq.XToolkit.WhiteLabel.Dialogs;
+using Softeq.XToolkit.WhiteLabel.Mvvm;
+using Softeq.XToolkit.WhiteLabel.Navigation;
+
+namespace InternalLibrary.ViewModels
+{
+    public abstract class SimpleListViewModelBase : ViewModelBase
+    {
+        private readonly IDialogsService _dialogsService;
+
+        public SimpleListViewModelBase(
+            IDialogsService dialogsService)
+        {
+            _dialogsService = dialogsService;
+
+            ItemModels = new ObservableRangeCollection<BookViewModel>();
+
+            SelectItemCommand = new AsyncCommand<BookViewModel>(SelectItem);
+        }
+
+        public ObservableRangeCollection<BookViewModel> ItemModels { get; }
+
+        public ICommand<BookViewModel> SelectItemCommand { get; }
+
+        public override void OnInitialize()
+        {
+            base.OnInitialize();
+
+            ItemModels.AddRange(new List<BookViewModel>
+            {
+                new BookViewModel()
+                {
+                    Name = "First book",
+                    InternationalStandardBookNumber = String.Empty,
+                    DateOfIssue = DateTime.Now,
+                    Location = String.Empty
+                },
+                new BookViewModel()
+                {
+                    Name = "Second book",
+                    InternationalStandardBookNumber = String.Empty,
+                    DateOfIssue = DateTime.Now,
+                    Location = String.Empty
+                },
+                new BookViewModel()
+                {
+                    Name = "Third book",
+                    InternationalStandardBookNumber = String.Empty,
+                    DateOfIssue = DateTime.Now,
+                    Location = String.Empty
+                }
+            });
+        }
+
+        private async Task SelectItem(BookViewModel viewModel)
+        {
+            await _dialogsService.ShowDialogAsync(new AlertDialogConfig("Selected", viewModel.Name, "OK"));
+        }
+    }
+}
