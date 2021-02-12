61f174e584d7cb09ecae1f8136aec5872124f9ac
AndreySavitsky <Andrey.Savitsky@softeq.com>
2021-02-12 11:54:47 +0300

    BookViewController added

---
+++ iOS/ViewControllers/BookViewController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using InternalLibrary.ViewModels;
+
+using Softeq.XToolkit.WhiteLabel.iOS;
+
+namespace InternalLibrary.iOS.ViewControllers
+{
+    public partial class BookViewController : ViewControllerBase<BookViewModel>
+    {
+        public BookViewController(IntPtr handle) : base(handle)
+        {
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+    }
+}
+
