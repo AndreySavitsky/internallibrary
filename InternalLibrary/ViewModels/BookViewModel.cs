eea41a030b133c0ba74f96dacdca8c9c021aa68f
AndreySavitsky <Andrey.Savitsky@softeq.com>
2021-02-12 12:18:36 +0300

    BookViewModel refactoring

--- InternalLibrary/ViewModels/BookViewModel.cs
+++ InternalLibrary/ViewModels/BookViewModel.cs
@@ -1,38 +1,12 @@
-﻿using System;
-
-using Softeq.XToolkit.Common;
+﻿using InternalLibrary.Models;
+using InternalLibrary.Services;
+using Softeq.XToolkit.WhiteLabel.Mvvm;
+using Softeq.XToolkit.WhiteLabel.Navigation;
 
 namespace InternalLibrary.ViewModels
 {
-    public class BookViewModel : ObservableObject
+    public class BookViewModel : ViewModelBase
     {
-        private string _title = string.Empty;
-        private DateTime _dateOfIssue = default;
-        private string _internationalStandardBookNumber = string.Empty;
-        private string _location = string.Empty;
-
-        public string Title
-        {
-            get => _title;
-            set => Set(ref _title, value);
-        }
-
-        public DateTime DateOfIssue
-        {
-            get => _dateOfIssue;
-            set => Set(ref _dateOfIssue, value);
-        }
-
-        public string InternationalStandardBookNumber
-        {
-            get => _internationalStandardBookNumber;
-            set => Set(ref _internationalStandardBookNumber, value);
-        }
-
-        public string Location
-        {
-            get => _location;
-            set => Set(ref _location, value);
-        }
+        public Book Book { get; set; }
     }
 }
