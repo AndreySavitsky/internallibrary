7edcdb34357a2703769fcd300bfdd6b28357e6e3
AndreySavitsky <Andrey.Savitsky@softeq.com>
2021-02-12 12:18:17 +0300

    SimpleListViewModelBase deleted

--- InternalLibrary/ViewModels/BookListViewModel.cs
+++ InternalLibrary/ViewModels/BookListViewModel.cs
@@ -1,25 +1,49 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+
+using InternalLibrary.Models;
 using InternalLibrary.Services;
+
+using Softeq.XToolkit.Common.Collections;
+using Softeq.XToolkit.Common.Commands;
+using Softeq.XToolkit.WhiteLabel.Mvvm;
 using Softeq.XToolkit.WhiteLabel.Navigation;
 
 namespace InternalLibrary.ViewModels
 {
-    public class BookListViewModel : SimpleListViewModelBase
+    public class BookListViewModel : ViewModelBase
     {
+        private readonly IBookRepository _bookRepository;
         private readonly IPageNavigationService _pageNavigationService;
 
         public BookListViewModel(
             IPageNavigationService pageNavigationService,
-            IDialogsService dialogsService,
             IBookRepository bookRepository)
-            : base(dialogsService, bookRepository)
         {
             _pageNavigationService = pageNavigationService;
+            _bookRepository = bookRepository;
+
+            ItemModels = new ObservableRangeCollection<Book>();
+
+            SelectItemCommand = new AsyncCommand<Book>(SelectItem);
         }
 
-        public override void OnInitialize()
+        public ObservableRangeCollection<Book> ItemModels { get; }
+
+        public ICommand<Book> SelectItemCommand { get; }
+
+        public override async void OnInitialize()
         {
             base.OnInitialize();
+
+            ItemModels.AddRange(await _bookRepository.GetBookListAsync());
+        }
+
+        private async Task SelectItem(Book model)
+        {
+            _pageNavigationService
+                .For<BookViewModel>()
+                .WithParam(x => x.Book, model)
+                .Navigate();
         }
     }
 }
