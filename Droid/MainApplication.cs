1f21a665d8f27e6aa01bb12d8a9597a43dd476bb
Андрей Савицкий <Lessthree@mail.ru>
2021-02-25 12:05:26 +0300

    Main Application added

---
+++ Droid/MainApplication.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using Softeq.XToolkit.WhiteLabel.Bootstrapper;
+using Softeq.XToolkit.WhiteLabel.Droid;
+
+namespace InternalLibrary.Droid
+{
+#if DEBUG
+    [Application(Debuggable = true)]
+#else
+    [Application(Debuggable = false)]
+#endif
+    public class MainApplication : MainApplicationBase
+    {
+        protected MainApplication(IntPtr handle, JniHandleOwnership transer)
+            : base(handle, transer)
+        {
+        }
+
+        protected override IBootstrapper CreateBootstrapper()
+        {
+            return new CustomDroidBootstrapper();
+        }
+    }
+}
+
