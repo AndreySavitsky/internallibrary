247630b5f84b380fbfb545e9fe4b1f5e546ae79f
Андрей Савицкий <Lessthree@mail.ru>
2021-02-25 12:06:34 +0300

    Book list page activity added

---
+++ Droid/Views/BookListPageActivity.cs
@@ -0,0 +1,13 @@
+﻿using Android.App;
+using InternalLibrary.ViewModels;
+
+using Softeq.XToolkit.WhiteLabel.Droid;
+
+namespace InternalLibrary.Droid.Views
+{
+    [Android.App.Activity(Label = "BookListPageActivity", MainLauncher = true)]
+    public class BookListPageActivity : Activity
+    {
+        
+    }
+}
