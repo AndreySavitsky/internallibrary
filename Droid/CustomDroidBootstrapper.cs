74de2b7d0f9e0d68a8335bc2de65c0666b09eb59
Андрей Савицкий <Lessthree@mail.ru>
2021-02-25 12:05:46 +0300

    Custor Droid Bootstrapper added

---
+++ Droid/CustomDroidBootstrapper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+using InternalLibrary.Services;
+
+
+using Softeq.XToolkit.WhiteLabel.Droid;
+using Softeq.XToolkit.Common.Extensions;
+using Softeq.XToolkit.WhiteLabel.Bootstrapper.Abstract;
+using Softeq.XToolkit.WhiteLabel.Droid.Services;
+using Softeq.XToolkit.WhiteLabel.Interfaces;
+using Softeq.XToolkit.WhiteLabel.Navigation;
+using Softeq.XToolkit.WhiteLabel.Droid.Dialogs;
+
+namespace InternalLibrary.Droid
+{
+    public class CustomDroidBootstrapper : DroidBootstrapperBase
+    {
+        protected override IList<Assembly> SelectAssemblies()
+        {
+            return base.SelectAssemblies() // Softeq.XToolkit.WhiteLabel.Droid
+                .AddItem(GetType().Assembly); // Playground.Droid
+        }
+
+        protected override void ConfigureIoc(IContainerBuilder builder)
+        {
+            // core
+            CustomBootstrapper.Configure(builder);
+
+            builder.Singleton<DroidAppInfoService, IAppInfoService>();
+
+            builder.Singleton<AndroidGoogleAPIConfiguration, IGoogleAPIConfoguration>();
+
+            builder.Singleton<DroidFragmentDialogService, IDialogsService>();
+        }
+    }
+}
