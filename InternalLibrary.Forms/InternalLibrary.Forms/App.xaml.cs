2f49103b00029d755f011346a64191361cca6494
Андрей Савицкий <Lessthree@mail.ru>
2021-03-10 16:15:30 +0300

    BookListView added

--- InternalLibrary.Forms/InternalLibrary.Forms/App.xaml.cs
+++ InternalLibrary.Forms/InternalLibrary.Forms/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Softeq.XToolkit.WhiteLabel.Bootstrapper;
+﻿using InternalLibrary.Forms.Views;
+using Softeq.XToolkit.WhiteLabel.Bootstrapper;
 using Softeq.XToolkit.WhiteLabel.Bootstrapper.Abstract;
 using Softeq.XToolkit.WhiteLabel.Navigation;
 using Xamarin.Forms;
@@ -11,7 +12,7 @@ namespace InternalLibrary.Forms
             : base(bootstrapper)
         {
             InitializeComponent();
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new BookListView());
         }
 
         protected override void OnStarted(IContainer container)
---
+++ InternalLibrary.Forms/InternalLibrary.Forms/Views/BookListView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace InternalLibrary.Forms.Views
+{
+    public partial class BookListView : ContentPage
+    {
+        public BookListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
