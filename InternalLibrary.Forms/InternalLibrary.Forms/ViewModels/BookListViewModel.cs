7c3ae5e36535b26adffd635ae3d11c2235afa651
Андрей Савицкий <Lessthree@mail.ru>
2021-03-10 22:54:06 +0300

    BookListViewModel added

---
+++ InternalLibrary.Forms/InternalLibrary.Forms/ViewModels/BookListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Softeq.XToolkit.WhiteLabel.Mvvm;
+using Softeq.XToolkit.WhiteLabel.Navigation;
+
+namespace InternalLibrary.Forms.ViewModels
+{
+    public class BookListViewModel : ViewModelBase
+    {
+        private readonly IPageNavigationService _pageNavigationService;
+
+        private string _title = string.Empty;
+
+        public BookListViewModel(IPageNavigationService pageNavigationService)
+        {
+            _pageNavigationService = pageNavigationService;
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        public override void OnInitialize()
+        {
+            base.OnInitialize();
+        }
+    }
+}
