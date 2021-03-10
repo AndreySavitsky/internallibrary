9370722e96681cdc93b5dce2489c5c845057ab6b
Андрей Савицкий <Lessthree@mail.ru>
2021-03-10 22:54:25 +0300

    InternalLibraryViewLocator added

---
+++ InternalLibrary.Forms/InternalLibrary.Forms/Servsices/InternalLibraryViewLocator.cs
@@ -0,0 +1,15 @@
+﻿using Softeq.XToolkit.WhiteLabel.Forms.Navigation;
+
+namespace InternalLibrary.Forms.Servsices
+{
+    public class InternalLibraryViewLocator : FormsViewLocator
+    {
+        protected override string BuildPageTypeName(string viewModelTypeName)
+        {
+            var name = viewModelTypeName
+                .Replace(".ViewModels.", ".Views.")
+                .Replace("Model", string.Empty);
+            return name;
+        }
+    }
+}
