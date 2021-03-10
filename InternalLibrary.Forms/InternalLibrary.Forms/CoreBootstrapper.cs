f172432b45f5a941ef66032b7b457f6be3f22b12
Андрей Савицкий <Lessthree@mail.ru>
2021-03-10 11:32:08 +0300

    Create CoreBootstrapper.cs

---
+++ InternalLibrary.Forms/InternalLibrary.Forms/CoreBootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Softeq.XToolkit.WhiteLabel.Bootstrapper.Abstract;
+using Softeq.XToolkit.WhiteLabel.Forms;
+using Xamarin.Forms;
+
+namespace InternalLibrary.Forms
+{
+    public class CoreBootstrapper : FormsBootstrapper
+    {
+        protected override IList<Assembly> SelectAssemblies()
+        {
+            return new List<Assembly>
+            {
+                typeof(App).Assembly
+            };
+        }
+
+        protected override bool IsExtractToAssembliesCache(Type type)
+        {
+            return typeof(Page).IsAssignableFrom(type);
+        }
+
+        protected override void ConfigureIoc(IContainerBuilder builder)
+        {
+
+        }
+    }
+}
