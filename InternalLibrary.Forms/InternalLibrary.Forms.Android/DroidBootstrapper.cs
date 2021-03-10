4df0fe78a6327853d1dcbe3c3088a1cbb61d5416
Андрей Савицкий <Lessthree@mail.ru>
2021-03-10 12:44:36 +0300

    DroidBootstrapper added

---
+++ InternalLibrary.Forms/InternalLibrary.Forms.Android/DroidBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Softeq.XToolkit.Common.Extensions;
+using Softeq.XToolkit.WhiteLabel.Bootstrapper.Abstract;
+
+namespace InternalLibrary.Forms.Droid
+{
+    public class DroidBootstrapper : CoreBootstrapper
+    {
+        protected override IList<Assembly> SelectAssemblies()
+        {
+            return base.SelectAssemblies()
+                .AddItem(GetType().Assembly);
+        }
+
+        protected override void ConfigureIoc(IContainerBuilder builder)
+        {
+            base.ConfigureIoc(builder);
+        }
+    }
+}
