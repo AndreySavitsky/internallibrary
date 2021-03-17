7bcfa97ccadb7c6abd462ce5f8b0736bce6ff69c
Андрей Савицкий <Lessthree@mail.ru>
2021-03-17 15:53:35 +0300

    WebAuthenticator platform specific setup

---
+++ InternalLibrary.Forms/InternalLibrary.Forms.Android/Activities/WebAuthenticationCallbackActivity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+
+namespace InternalLibrary.Forms.Droid.Activities
+{
+    [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+        Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
+        DataScheme = "com.softeq.internallibrary")]
+    public class WebAuthenticationCallbackActivity : Xamarin.Essentials.WebAuthenticatorCallbackActivity
+    {
+    }
+}
