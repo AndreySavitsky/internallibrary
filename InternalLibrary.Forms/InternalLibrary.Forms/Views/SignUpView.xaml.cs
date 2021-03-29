865da751c88db96f46334287ab67bf8a21926ad5
Андрей Савицкий <Lessthree@mail.ru>
2021-03-29 15:59:07 +0300

    Create SignUpView.xaml

---
+++ InternalLibrary.Forms/InternalLibrary.Forms/Views/SignUpView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace InternalLibrary.Forms.Views
+{
+    public partial class SignUpView : ContentPage
+    {
+        public SignUpView()
+        {
+            InitializeComponent();
+        }
+    }
+}
