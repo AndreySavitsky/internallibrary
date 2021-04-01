aa57a1cd8e348229b7488385ecb8ce92f8cce0d3
Андрей Савицкий <Lessthree@mail.ru>
2021-04-01 19:07:48 +0300

    IFireBaseDatabase interface added

---
+++ InternalLibrary.Forms/InternalLibrary.Forms/Servsices/IFirebaseDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using InternalLibrary.Forms.Models;
+
+namespace InternalLibrary.Forms.Servsices
+{
+    public interface IFirebaseDatabase
+    {
+        Task<Dictionary<string, Book>>  GetBooksAsync();
+        Task<Book> SetBookAsync(Book book);
+        Task<Book> PushBookAsync(Book book);
+        Task<Book> GetBookAsync(string book);
+        Task<Book> UpdateBookAsync(Book book);
+        void DeleteBookAsync(Book book);
+    }
+}
