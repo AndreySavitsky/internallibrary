fa11001f7f43f0fc6e6d5f9d465e2016f5081c6e
Андрей Савицкий <Lessthree@mail.ru>
2021-04-02 16:49:05 +0300

    ViewBase added

---
+++ InternalLibrary.Forms/InternalLibrary.Forms/Views/ViewBase.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Softeq.XToolkit.WhiteLabel.Mvvm;
+using Xamarin.Forms;
+
+namespace InternalLibrary.Forms.Views
+{
+    public partial class ViewBase : ContentPage
+    {
+        public ViewBase()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            (BindingContext as ViewModelBase).OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            (BindingContext as ViewModelBase).OnDisappearing();
+        }
+    }
+}
