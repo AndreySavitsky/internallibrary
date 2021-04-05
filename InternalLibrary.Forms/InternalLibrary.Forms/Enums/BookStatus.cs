d1928c3ca71ef8300d2a3351d3661c03e60e5521
Андрей Савицкий <Lessthree@mail.ru>
2021-04-05 16:26:30 +0300

    Book status constants added

---
+++ InternalLibrary.Forms/InternalLibrary.Forms/Enums/BookStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace InternalLibrary.Forms.Enums
+{
+    public static class BookStatus
+    {
+        public const string Free = "Free";
+        public const string Booked = "Booked";
+        public const string Borrowed = "Borrowed";
+        public const string Lost = "Lost";
+    }
+}
