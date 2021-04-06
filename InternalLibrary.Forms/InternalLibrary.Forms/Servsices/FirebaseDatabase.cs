42517e3cb96ba32bc3cbd7397e71b2d26a807408
AndreySavitsky <Andrey.Savitsky@softeq.com>
2021-04-06 13:46:09 +0300

    Get admins method added

--- InternalLibrary.Forms/InternalLibrary.Forms/Servsices/FirebaseDatabase.cs
+++ InternalLibrary.Forms/InternalLibrary.Forms/Servsices/FirebaseDatabase.cs
@@ -2,13 +2,14 @@
 using System.Threading.Tasks;
 using FireSharp;
 using FireSharp.Config;
-using FireSharp.Response;
 using InternalLibrary.Forms.Models;
 
 namespace InternalLibrary.Forms.Servsices
 {
     public class FirebaseDatabase : IFirebaseDatabase
     {
+        private const string _adminRepository = "Admins";
+        private const string _bookRepository = "Books";
         private readonly FirebaseClient _client;
 
         public FirebaseDatabase()
@@ -20,9 +21,17 @@ namespace InternalLibrary.Forms.Servsices
             });
         }
 
+        public async Task<Dictionary<string, string>> GetAdminsAsync()
+        {
+            var response = await _client.GetAsync(_adminRepository);
+            var admins = response.ResultAs<Dictionary<string, string>>();
+
+            return admins;
+        }
+
         public async Task<Dictionary<string, Book>> GetBooksAsync()
         {
-            FirebaseResponse response = await _client.GetAsync("Books");
+            var response = await _client.GetAsync(_bookRepository);
             var books = response.ResultAs<Dictionary<string, Book>>();
 
             return books;
@@ -30,31 +39,31 @@ namespace InternalLibrary.Forms.Servsices
 
         public async Task<Book> SetBookAsync(Book book)
         {
-            SetResponse response = await _client.SetAsync("Books/" + book.Title, book);
+            var response = await _client.SetAsync(_bookRepository + "/" + book.Title, book);
             return response.ResultAs<Book>();
         }
 
         public async Task<Book> PushBookAsync(Book book)
         {
-            PushResponse response = await _client.PushAsync("Books/" + book.Title, book);
+            var response = await _client.PushAsync(_bookRepository + "/" + book.Title, book);
             return response.ResultAs<Book>();
         }
 
         public async Task<Book> GetBookAsync(string book)
         {
-            FirebaseResponse response = await _client.GetAsync("Books/" + book);
+            var response = await _client.GetAsync(_bookRepository + "/" + book);
             return response.ResultAs<Book>();
         }
 
         public async Task<Book> UpdateBookAsync(Book book)
         {
-            FirebaseResponse response = await _client.UpdateAsync("Books/" + book.Title, book);
+            var response = await _client.UpdateAsync(_bookRepository + "/" + book.Title, book);
             return response.ResultAs<Book>();
         }
 
         public async void DeleteBookAsync(Book book)
         {
-            await _client.DeleteAsync("Books/" + book.Title);
+            await _client.DeleteAsync(_bookRepository + "/" + book.Title);
         }
     }
 }
--- InternalLibrary.Forms/InternalLibrary.Forms/Servsices/IFirebaseDatabase.cs
+++ InternalLibrary.Forms/InternalLibrary.Forms/Servsices/IFirebaseDatabase.cs
@@ -6,6 +6,7 @@ namespace InternalLibrary.Forms.Servsices
 {
     public interface IFirebaseDatabase
     {
+        Task<Dictionary<string, string>> GetAdminsAsync();
         Task<Dictionary<string, Book>>  GetBooksAsync();
         Task<Book> SetBookAsync(Book book);
         Task<Book> PushBookAsync(Book book);
